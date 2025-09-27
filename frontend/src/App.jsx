d853860cac723aa301ae5d8341f4ee6c7febff63
rahidkhan <rahidkhanbannu1@email.com>
2025-09-27 22:44:06 +0430

    create sign up form ui and functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import { BrowserRouter , Routes , Route  } from "react-router-dom"
+import React from "react"
+import "./App.css"
+import SignUp from "./pages/SignUp/signUp"
+import Signin from "./pages/SignIn/Signin"
+import Filter from "./pages/Filter/Filter"
+import Home from "./pages/Home/Home"
+
+
 
 function App() {
   return (
-    <div>
-      <h1>hellow world</h1>
-    </div>
+        <BrowserRouter>
+        <Routes>
+           <Route path="/" element={<Home/>} />
+           <Route path="/sign-in" element={<Signin/>} />
+           <Route path="/sign-up" element={<SignUp/>} />
+           <Route path="/filter" element={<Filter/>} />
+
+        </Routes>
+        </BrowserRouter>
   )
 }
 
---
+++ frontend/src/pages/SignUp/signUp.jsx
@@ -0,0 +1,138 @@
+import React, { useState } from "react";
+import { Box, TextField , formControlClasses , FormControl, Button } from "@mui/material";
+
+
+function SignUp() {
+  const [form, setForm] = useState({
+  });
+
+  const [errors, setErrors] = useState({
+    firstname:"",
+    lastname:"",
+    username:"",
+    email:"",
+    password:"",
+    confirmpassword:"",
+  });
+
+ 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+   console.log(form);
+  };
+
+  return (
+    <>
+      <FormControl
+      
+        className="userForm"
+        sx={{
+          paddingBlock: "30px",
+          borderRadius: "10px",
+          paddingInline: "10px",
+        }}
+      >
+        <h1> Sign up </h1>
+        <Box sx={{ width: "100%" }}>
+          <Box sx={{ display: "flex", gap: "10px", width: "100%" }}>
+            <TextField
+              label="First name"
+              fullWidth
+              onChange={(e) =>
+                setForm({ ...form, [e.target.id]: e.target.value })
+              }
+              type="text"
+              value={form.firstname}
+              sx={{ marginBottom: "10px", borderRadius: "10px" }}
+              id="firstname"
+            
+            />
+            <TextField
+              label="Last name"
+              fullWidth
+              type="text"
+              sx={{ marginBottom: "10px", borderRadius: "10px" }}
+              onChange={(e) =>
+                setForm({ ...form, [e.target.id]: e.target.value })
+              }
+              value={form.lastname}
+              id="lastname"
+             
+            />
+          </Box>
+          <Box sx={{ display: "flex", gap: "10px", width: "100%" }}>
+            <TextField
+              label="username"
+              type="username"
+              fullWidth
+              sx={{ marginBottom: "10px", borderRadius: "10px" }}
+              onChange={(e) =>
+                setForm({ ...form, [e.target.id]: e.target.value })
+              }
+              id="username"
+              value={form.username}
+            
+            />
+            <TextField
+              label="email"
+              type="email"
+              fullWidth
+              sx={{ marginBottom: "10px", borderRadius: "10px" }}
+              onChange={(e) =>
+                setForm({ ...form, [e.target.id]: e.target.value })
+              }
+              id="email"
+              value={form.email}
+            
+            />
+          </Box>
+          <Box sx={{ display: "flex", gap: "10px", width: "100%" }}>
+            <TextField
+              label="password"
+              type="password"
+              fullWidth
+              sx={{ marginBottom: "10px", borderRadius: "10px" }}
+              onChange={(e) =>
+                setForm({ ...form, [e.target.id]: e.target.value })
+              }
+              value={form.password}
+              id="password"
+         
+            />
+            <TextField
+              label="confirm password"
+              type="text"
+              fullWidth
+              sx={{ marginBottom: "10px", borderRadius: "10px" }}
+              onChange={(e) =>
+                setForm({ ...form, [e.target.id]: e.target.value })
+              }
+              id="conformpassword"
+              value={form.confirmpassword}
+           
+            />
+          </Box>
+          <Box fullWidth sx={{display:"flex", justifyContent:"center", alignItems:"center"}}>
+            <Button
+              onClick={(e)=>handleSubmit(e)}
+              variant="outlined"
+              sx={{
+                background: "blue",
+                padding: "15px",
+                color: "white",
+                marginTop: "10px",
+                fontSize:"17px",
+                width:"80%"
+
+              }}
+            >
+              Sign up
+            </Button>
+          </Box>
+        </Box>
+      </FormControl>
+    </>
+  );
+}
+
+export default SignUp;
