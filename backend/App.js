33de0f9e38108ad800c088624f43b3092a50dae3
rahidkhan <rahidkhanbannu1@email.com>
2025-09-28 16:29:09 +0430

    setup auth routers

--- backend/App.js
+++ backend/App.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const AuthRouter  = require("./src/routers/auth.routes.js");
+
 function createApp(){
      const App = express();
      App.use(express.json());
      App.use(express.urlencoded({extended:true}));
+     App.use('/api/auth/',AuthRouter)
      return  App;
 }
 
