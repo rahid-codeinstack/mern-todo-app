d678c8192d4ffd9c8f234e2e162d0e0a1e90e5d5
rahidkhan <rahidkhanbannu1@email.com>
2025-09-28 15:54:02 +0430

    create sign in form ui

--- frontend/src/pages/SignIn/Signin.jsx
+++ frontend/src/pages/SignIn/Signin.jsx
@@ -1,11 +1,92 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Box, TextField , formControlClasses , FormControl, Button } from "@mui/material";
 
-function Signin() {
+
+function SignIn() {
+  const [form, setForm] = useState({
+    email:"",
+    password:"",
+  });
+
+  const [errors, setErrors] = useState({
+    email:"",
+    password:"",
+  });
+
+ 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+   console.log(form);
+  };
+
   return (
-    <div>
-      signin
-    </div>
-  )
+    <>
+      <FormControl
+        className="userForm user-signin"
+        sx={{
+          paddingBlock: "30px",
+          borderRadius: "10px",
+          paddingInline: "10px",
+        }}
+      >
+        <h1 className="form-header">Sign Up</h1>
+        <Box sx={{ width: "100%" }}>
+          <Box sx={{ display: "flex", gap: "10px", width: "100%" }}>
+            <TextField
+              label="Email"
+              fullWidth
+              onChange={(e) =>
+                setForm({ ...form, [e.target.id]: e.target.value })
+              }
+              type="text"
+              value={form.email}
+              sx={{ marginBottom: "10px", borderRadius: "10px" }}
+              id="email"
+            />
+          </Box>
+          <Box>
+            <TextField
+              label="password"
+              fullWidth
+              type="password"
+              required
+              sx={{ marginBottom: "10px", borderRadius: "10px" }}
+              onChange={(e) =>
+                setForm({ ...form, [e.target.id]: e.target.value })
+              }
+              value={form.password}
+              id="password"
+            />
+          </Box>
+
+          <Box
+            fullWidth
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Button
+              onClick={(e) => handleSubmit(e)}
+              variant="outlined"
+              sx={{
+                background: "blue",
+                padding: "15px",
+                color: "white",
+                marginTop: "10px",
+                fontSize: "17px",
+                width: "80%",
+              }}
+            >
+             Sign In
+            </Button>
+          </Box>
+        </Box>
+      </FormControl>
+    </>
+  );
 }
 
-export default Signin
+export default SignIn;
+
